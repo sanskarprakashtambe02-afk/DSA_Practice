4d0d645e10ed58398d1b71534139cafbcd01e6dd
sanskarprakashtambe02-afk <sanskarprakashtambe02@gmail.com>
2026-08-06 11:24:20 +0530

    Time: 1 ms (96.42%), Space: 17.7 MB (42.64%) - LeetHub

---
+++ 3941-password-strength/3941-password-strength.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int passwordStrength(string password) {
+        unordered_map<char,int>m;
+        for(int i=0;i<password.size();i++){
+            m[password[i]]++;
+        }
+        int ans=0;
+        for(auto&[key,count]:m){
+            int k=key;
+            if(k>=48&&k<=57){
+                ans=ans+3;
+            }
+            if(k>=97&&k<=122){
+                ans++;
+            }
+            if(k>=65&&k<=90){
+                ans+=2;
+            }
+            if(key=='!'||key=='@'||key=='#'||key=='$'){
+                ans+=5;
+            }
+        }
+        return ans;
+    }
+};
