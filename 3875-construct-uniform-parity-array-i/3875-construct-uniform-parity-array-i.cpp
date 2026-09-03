2f6b28d13303fdce37454ffbda93dc6424a4b7a0
sanskarprakashtambe02-afk <sanskarprakashtambe02@gmail.com>
2026-09-03 09:00:26 +0530

    Time: 0 ms (100%), Space: 30.4 MB (36.41%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
+        bool isodd=false;
+        for(int i=0;i<nums1.size();i++){
+            if(nums1[i]%2==1){
+                isodd=true;
+            }
+        }
+        if(isodd){
+            return true;
+        }
         return true;
     }
 };
